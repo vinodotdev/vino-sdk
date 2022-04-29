6e97744e34aa237bb527bc07aa3e98963351e504
Jarrod Overson <jsoverson@gmail.com>
2022-04-29 08:39:04 -0400

    added From<String> impl for WasmError

--- crates/vino-provider/src/wasm/error.rs
+++ crates/vino-provider/src/wasm/error.rs
@@ -69,6 +69,12 @@ impl From<&str> for Error {
   }
 }
 
+impl From<String> for Error {
+  fn from(e: String) -> Self {
+    Error::Component(e)
+  }
+}
+
 impl std::fmt::Display for Error {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
